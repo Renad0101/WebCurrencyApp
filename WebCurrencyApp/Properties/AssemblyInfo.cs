aa5ffbe7353f0bfebcc18564a2809ce197e78fb5
Renad0101 <renadimamaliyev@gmail.com>
2023-03-13 12:34:15 +0400

    WEb Currency

---
+++ WebCurrencyApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebCurrencyApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebCurrencyApp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7214d35e-4a8f-4761-aca7-ee6b6c8c84c9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebCurrencyApp/WebService1.asmx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace WebCurrencyApp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public XmlDocument GetCurrency (string currency_name)
+        {
+            string result = @"<root>{result}</root>";
+
+            XmlDocument Xmldoc = new XmlDocument();
+
+            string date = DateTime.Now.ToString("dd.MM.yyyy");
+
+            Xmldoc.Load("https://cbar.az/currencies/" + date + ".xml");
+
+            XmlNodeList Xmllist = Xmldoc.GetElementsByTagName("Valute");
+
+            foreach (XmlNode item in Xmllist) 
+            { 
+                if (item.Attributes[0].Value == currency_name)
+                {
+                    result = result.Replace("{result}", item.InnerXml);
+                }
+            }
+
+            XmlDocument Myxml = new XmlDocument();
+            Myxml.LoadXml(result);
+            return Myxml;   
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Currency - Mezenne
+    //    [WebMethod]
+
+    //    public XmlDocument GetCurrency (string currencyname)
+    //    {
+    //        string result = @"<root>{result}</root>"; // response model 
+
+
+    //     XmlDocument xmldoc = new XmlDocument();
+
+
+    //        string date = DateTime.Now.ToString("dd.MM.yyyy");
+
+
+    //        xmldoc.Load("https://cbar.az/currencies/" + date + ".xml");
+
+
+    //        XmlNodeList xmlist = xmldoc.GetElementsByTagName("Valute");
+
+    //        foreach (XmlNode item in xmlist)
+    //        {
+    //            if (item.Attributes[0].Value== currencyname)
+    //            {
+    //                result = result.Replace("{result}", item.InnerXml);
+    //            }
+    //        }
+
+    //        XmlDocument myxml = new XmlDocument();
+    //        myxml.LoadXml(result);
+    //        return myxml;   
+    //}
+
+}
+}
